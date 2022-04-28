06d1af9da03ac42344be0cf23c14a67893dde155
Laerte <laerteakira@gmail.com>
2022-04-28 11:21:03 -0300

    Connect with MongoDB

--- app.js
+++ app.js
@@ -1,38 +1,52 @@
 import express, { json } from 'express';
 import cors from 'cors';
 import Joi from 'joi';  
+import { MongoClient } from 'mongodb';
 
 const app = express();
 app.use(cors());
 app.use(json());
 
-const participantes = [];
+let database;
+const mongoClient = new MongoClient("mongodb://localhost:27017");
 
 app.post("/participants", async (req, res) => {
     const { name } = req.body;
-    const schema = Joi.string().trim().required().empty(" ");
+    const schema = Joi.string().trim().required();
 
     try{
+        await mongoClient.connect();
+        database = mongoClient.db("test");
+
         const value = await schema.validateAsync(name);
-        const jaExisteParticipante = participantes.find((participante) => {
-            return value === participante.name;
-        })
-        if(!jaExisteParticipante){
-            participantes.push({
-                name:value,
+        const existeParticipante = await database.collection('participants').findOne({name:value});
+        if(!existeParticipante){
+            await database.collection('participants').insertOne({
+                name: value,
                 lastStatus: Date.now()
             });
         } else{
             res.sendStatus(409);
         }
         res.sendStatus(201);
+        mongoClient.close();
     }catch(e){
         res.sendStatus(422);
+        mongoClient.close();
     }
 });
 
-app.get("/participants", (req, res) => {
-    res.send(participantes);
+app.get("/participants", async (req, res) => {
+    try{
+        await mongoClient.connect();
+        database = mongoClient.db("test");
+
+        const participantes = await database.collection('participants').find({}).toArray();
+        res.send(participantes);
+        mongoClient.close();
+    }catch(e){
+        res.send(e);
+    }
 })
 
 app.listen(5000);
